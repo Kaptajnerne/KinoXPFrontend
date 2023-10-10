afe9e719e54cae5bdc4048fd3a00f995b4193cd2
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-10-10 10:26:11 +0200

    refactoring

--- js/scheduletheater1.js
+++ js/scheduletheater1.js
@@ -1,143 +1,81 @@
+import {fetchAnyUrl} from "/js/modulejson.js";
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Sample data for booked seats (replace with your dynamic data)
-    const bookedSeats = [1, 2, 5, 8]; // Adjust seat numbers as needed
-    const numberOfSeats = 239;
 
-    // Function to generate dynamic seats
-    function generateSeats() {
-        let seats = document.querySelector(".all-seats");
-        for (let i = 1; i <= numberOfSeats; i++) {
-            let isBooked = bookedSeats.includes(i);
-            let bookedClass = isBooked ? "booked" : "";
-            let disabledAttr = isBooked ? "disabled" : "";
-            seats.insertAdjacentHTML(
-                "beforeend",
-                `<input type="checkbox" name="tickets" id="s${i}" ${disabledAttr} /><label for="s${i}" class="seat ${bookedClass}"></label>`
-            );
+    const seatsContainer = document.querySelector(".all-seats");
+
+    //Generate seats
+    function generateSeats(theater) {
+        const seatsPerLine = theater.seatsPrLine;
+        const numberOfLines = theater.numberOfLines;
+
+        for (let row = 1; row <= numberOfLines; row++) {
+            for (let seatNumber = 1; seatNumber <= seatsPerLine; seatNumber++) {
+                const seatId = `r${row}c${seatNumber}`;
+                const isBooked = false;
+                const bookedClass = isBooked ? "booked" : "";
+                const disabledAttr = isBooked ? "disabled" : "";
+
+                seatsContainer.insertAdjacentHTML(
+                    "beforeend",
+                    `<input type="checkbox" name="tickets" id="${seatId}" ${disabledAttr} />
+                    <label for="${seatId}" class="seat ${bookedClass}"></label>`
+                );
+            }
         }
+        //Calculate the number of columns for CSS
+        const totalSeats = seatsPerLine * numberOfLines;
+        const numColumns = Math.ceil(totalSeats / numberOfLines);
+        seatsContainer.style.gridTemplateColumns = `repeat(${numColumns}, 1fr)`;
 
-        // Update event listeners for dynamically generated seats
-        let tickets = seats.querySelectorAll("input");
-        tickets.forEach((ticket) => {
-            ticket.addEventListener("change", () => {
-                let amount = document.querySelector(".amount").innerHTML;
-                let count = document.querySelector(".count").innerHTML;
-                amount = Number(amount);
-                count = Number(count);
-
-                if (!ticket.disabled) {
-                    if (ticket.checked) {
-                        count += 1;
-                        amount += 150;
-                    } else {
-                        count -= 1;
-                        amount -= 150;
-                    }
-                    document.querySelector(".amount").innerHTML = amount;
-                    document.querySelector(".count").innerHTML = count;
-                }
-            });
-        });
     }
 
-    // Call the function to generate seats
-    generateSeats();
-});
-
-
-
-
-
-/*
-let seats = document.querySelector(".all-seats");
-for (var i = 0; i < 239; i++) {
-    let randint = Math.floor(Math.random() * 2);
-    let booked = randint === 1 ? "booked" : "";
-    seats.insertAdjacentHTML(
-        "beforeend",
-        '<input type="checkbox" name="tickets" id="s' +
-        (i + 2) +
-        '" /><label for="s' +
-        (i + 2) +
-        '" class="seat ' +
-        booked +
-        '"></label>'
-    );
-}
-
-let tickets = seats.querySelectorAll("input");
-tickets.forEach((ticket) => {
-    ticket.addEventListener("change", () => {
+    //Update seats for dynamic flow
+    function updateSeatCount() {
         let amount = document.querySelector(".amount").innerHTML;
         let count = document.querySelector(".count").innerHTML;
         amount = Number(amount);
         count = Number(count);
 
-        if (ticket.checked) {
-            count += 1;
-            amount += 150;
-        } else {
-            count -= 1;
-            amount -= 150;
-        }
-        document.querySelector(".amount").innerHTML = amount;
-        document.querySelector(".count").innerHTML = count;
-    });
+        return (ticket) => {
+            if (!ticket.disabled) {
+                if (ticket.checked) {
+                    count += 1;
+                    amount += 100;
+                } else {
+                    count -= 1;
+                    amount -= 100;
+                }
+                document.querySelector(".amount").innerHTML = amount;
+                document.querySelector(".count").innerHTML = count;
+            }
+        };
+    }
+
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const showtimeId = urlParams.get('showtimeId');
+
+    if (!showtimeId) {
+        console.error('Showtime ID not found in URL parameters.');
+    } else {
+
+        fetchAnyUrl(`http://localhost:8080/showtimes/${showtimeId}/theater`)
+            .then((theaterSize) => {
+                generateSeats(theaterSize);
+
+                //Update event listeners for dynamically generated seats
+                const tickets = seatsContainer.querySelectorAll("input");
+                const updateSeatCountFn = updateSeatCount();
+
+                tickets.forEach((ticket) => {
+                    ticket.addEventListener("change", () => {
+                        updateSeatCountFn(ticket);
+                    });
+                });
+            })
+            .catch((error) => {
+                console.error('Error fetching theater size:', error);
+            });
+    }
 });
-*/
-
-
-
-
-
-
-
-
-/*
-// Fetch movie and showtime data from your backend API
-fetch('your_backend_api_url')
-    .then(response => response.json())
-    .then(data => {
-        // Process the data and generate the schedule dynamically
-        generateSchedule(data);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
-
-// Function to generate the schedule based on the data
-function generateSchedule(data) {
-    const scheduleContainer = document.getElementById('schedule-container');
-
-    // Loop through the data to create schedule elements
-    data.forEach(movie => {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
-
-        // Create a title for the movie
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = movie.title;
-
-        // Create a table to display showtimes for each day
-        const tableElement = document.createElement('table');
-
-        // Loop through showtimes for each day
-        movie.showtimes.forEach(day => {
-            const row = tableElement.insertRow();
-            const cell1 = row.insertCell(0);
-            const cell2 = row.insertCell(1);
-
-            cell1.textContent = day.day;
-            cell2.textContent = day.showtime;
-        });
-
-        // Append title and table to the movie element
-        movieElement.appendChild(titleElement);
-        movieElement.appendChild(tableElement);
-
-        // Append the movie element to the schedule container
-        scheduleContainer.appendChild(movieElement);
-    });
-}
-
-*/
